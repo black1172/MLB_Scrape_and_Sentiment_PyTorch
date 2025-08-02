4feb3bcc05870832ebaf5aa3dc846a6c4c8e7a07
black1172 <black.1172@buckeyemail.osu.edu>
2025-08-02 16:48:03 -0400

    added scraping config

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
+# Config settings
 REDDIT_CONFIG = {
     'client_id': 'yr6C-BQ7ggjYuWIT-HRJ6A',
     'client_secret': 'bNkDxuK3NCA-_v3ye-A3PhzDewtOqA', 
     'user_agent': 'MLBContentIntelligence/1.0 by EnthusiasmLoud7786'
+}
+
+# Scraping settings
+SCRAPING_CONFIG = {
+    'subreddits': ['MLB', 'baseball'],
+    'posts_per_subreddit': 10,
+    'comments_per_post': 5
 }
