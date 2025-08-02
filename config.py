049ad9cd28535a1a5e2fcef5703a68b63036f53e
black1172 <black.1172@buckeyemail.osu.edu>
2025-08-02 16:41:29 -0400

    config finished for reddit API

---
+++ config.py
@@ -0,0 +1,5 @@
+REDDIT_CONFIG = {
+    'client_id': 'yr6C-BQ7ggjYuWIT-HRJ6A',
+    'client_secret': 'bNkDxuK3NCA-_v3ye-A3PhzDewtOqA', 
+    'user_agent': 'MLBContentIntelligence/1.0 by EnthusiasmLoud7786'
+}
